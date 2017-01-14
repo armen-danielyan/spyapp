9a9433684ffcb2e66e2bd4430acc6c8edd708341
Armen Danielyan <danielyan_armen@yahoo.com>
2017-01-14 17:26:10 +0400

    0.0.3

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.log(req.connection.remoteAddress);
-    res.render('index', {title: 'Express'});
+    //console.log(req.connection.remoteAddress);
+    res.render('index', {title: 'Express', ip: req.connection.remoteAddress});
 });
 
 module.exports = router;
