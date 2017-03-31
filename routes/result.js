407333a6b39d02bd929fe3ebd78935428460c318
Armen Danielyan <danielyan_armen@yahoo.com>
2017-03-31 21:29:38 +0400

    keep db connection alive

--- routes/result.js
+++ routes/result.js
@@ -3,21 +3,25 @@ var express = require('express');
 var router = express.Router();
 var config = require('config');
 
-var conn = mysql.createConnection(config.get('mysql'));
+//var conn = mysql.createConnection(config.get('mysql'));
+var mysqlPool  = mysql.createPool(config.get('mysql'));
 
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-
-    var q = 'SELECT *, COUNT(ip) as count, max(created_at) as created FROM users GROUP BY ip ORDER BY created_at DESC';
-
-    conn.query(q, function (error, results, fields) {
-        if (error) throw error;
-        return res.render('result', {title: 'Result', data: results});
+    var strQuery = 'SELECT *, COUNT(ip) as count, max(created_at) as created FROM users GROUP BY ip ORDER BY created_at DESC';
+    mysqlPool.getConnection(function(err, conn) {
+        if(err) throw err;
+        conn.query(strQuery, function(err, rows) {
+            if(err) {
+                conn.release();
+                console.error(err);
+                return;
+            }
+            res.render('result', {title: 'Result', data: rows});
+            conn.release();
+        });
     });
-
-
-
 });
 
 module.exports = router;
